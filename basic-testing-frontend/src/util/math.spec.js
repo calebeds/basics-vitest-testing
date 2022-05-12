4aeadc6296989f96c9f41bc4cfefe4d9c1d02cc5
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-12 15:02:35 -0300

    finish tests at math.spec.js

--- basic-testing-frontend/src/util/math.spec.js
+++ basic-testing-frontend/src/util/math.spec.js
@@ -18,10 +18,13 @@ it('should summarize all numbers values in an array', () => {
 });
 
 it('should yield NaN if at least one invalid number is provided', () => {
+    //Arrange
     const inputs = ['invalid', 1];
 
+    //Act
     const result = add(inputs);
 
+    //Assert
     expect(result).toBeNaN();
 });
 
@@ -36,4 +39,32 @@ it('should yield a correct if an array of numeric string values is provided', ()
             0
         );
     expect(result).toBe(expectedResult);
-})
+});
+
+it('should yield 0 if an empty array is provided', () => {
+    const numbers = [];
+
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+});
+
+it('should throw an error if no values is passed into the function', () => {
+    
+    const resultFn = () => {
+        add();
+    };
+
+    expect(resultFn).toThrow(/is not iterable/);
+});
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+    const num1 = 1;
+    const num2 = 2;
+    
+    const resultFn = () => {
+        add(num1, num2);
+    };
+
+    expect(resultFn).toThrow(/is not iterable/);
+});
