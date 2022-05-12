619261ebd4ff57cd379251a1e9b4b47abea964b5
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-12 14:09:16 -0300

    write number conversion tests and fixup math.js

--- basic-testing-frontend/src/util/math.spec.js
+++ basic-testing-frontend/src/util/math.spec.js
@@ -8,7 +8,7 @@ it('should summarize all numbers values in an array', () => {
         .reduce (
             (prevValue, curValue) => { return prevValue + curValue },
             0
-        );
+        );//This techinically can be put here or under the assert part 
 
     //Act
     const result = add(numbers);
@@ -16,3 +16,24 @@ it('should summarize all numbers values in an array', () => {
     //Assert
     expect(result).toBe(expectedResult);
 });
+
+it('should yield NaN if at least one invalid number is provided', () => {
+    const inputs = ['invalid', 1];
+
+    const result = add(inputs);
+
+    expect(result).toBeNaN();
+});
+
+it('should yield a correct if an array of numeric string values is provided', () => {
+    const strNumbers = ['1', '2'];
+
+    const result = add(strNumbers);
+
+    const expectedResult = strNumbers
+        .reduce (
+            (prevValue, curValue) => { return +prevValue + +curValue },
+            0
+        );
+    expect(result).toBe(expectedResult);
+})
