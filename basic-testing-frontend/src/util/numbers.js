cdca05e3e08ffbb771d29c3cca50e3b6107466bc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-13 15:57:19 -0300

    add some integration tests

--- basic-testing-frontend/src/util/numbers.js
+++ basic-testing-frontend/src/util/numbers.js
@@ -7,10 +7,14 @@ export function transformToNumber(value) {
 export function cleanNumbers(numberValues) {
   const numbers = [];
   for (const numberInput of numberValues) {
-    validateStringNotEmpty(numberInput);
-    const number = transformToNumber(numberInput);
-    validateNumber(number);
-    numbers.push(number);
+    if(typeof numberInput !== 'number'){ //An array of string and numbers can be provided
+      validateStringNotEmpty(numberInput);
+      const number = transformToNumber(numberInput);
+      validateNumber(number);
+      numbers.push(number);
+    } else { 
+      numbers.push(numberInput);
+    }
   }
 
   return numbers;
